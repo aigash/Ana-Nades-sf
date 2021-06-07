dfb5e3b2f053405862841352121755d536f71463
aigash <nicolas.florentino00@gmail.com>
2021-06-07 11:38:00 +0200

    fixtures 2 for spot

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,28 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Spot;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        $nbPostsToCreate = random_int(0, 10);
+
+        for ($j = 0; $j < $nbPostsToCreate; $j++) {
+            $onePost = new Spot();
+            $onePost->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+            $onePost->setTitle($faker->word());
+            $onePost->setContent($faker->word());
+            $onePost->setUrlPos($faker->imageUrl(640, 480, "cats", true, null, true));
+            $onePost->setUrlAim($faker->imageUrl(640, 480, "cats", true, null, true));
+            $onePost->setUrlLand($faker->imageUrl(640, 480, "cats", true, null, true));
+        }
 
         $manager->flush();
     }
